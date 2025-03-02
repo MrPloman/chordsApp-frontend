78b7a965f42d3daa9fa0e9aa90f0d64794fac1e2
MrPloman <polplanaayet@gmail.com>
2025-03-02 13:30:44 -0600

    modified audio location inside folder assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,20 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chordsApp-frontend';
+
+  soundSrc = './assets/audios/A5/A5_0_A.mp3';
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+  public sound() {
+    const audio = new Audio(this.soundSrc);
+    audio.load();
+    audio.play();
+  }
+  constructor() {}
 }
