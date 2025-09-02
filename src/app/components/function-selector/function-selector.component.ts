e114b26941ff3db975620f5b95500cb0dbb8b056
MrPloman <polplanaayet@gmail.com>
2025-09-01 20:06:07 -0500

    prevent message in status selection

--- src/app/components/function-selector/function-selector.component.ts
+++ src/app/components/function-selector/function-selector.component.ts
@@ -6,6 +6,8 @@ import { Subscription } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { selectChordGuesserState } from '../../store/selectors/chords.selector';
+import { Chord } from '@app/models/chord.model';
 
 @Component({
   selector: 'app-function-selector',
@@ -17,9 +19,18 @@ import { MatIconModule } from '@angular/material/icon';
 export class FunctionSelectorComponent {
   private store = inject(Store);
   private functionSelectedStoreSubscription: Subscription = new Subscription();
+  private chordGuesserSubscription: Subscription = new Subscription();
 
   public functionSelectedStore = this.store.select(selectFunctionSelectedState);
+  public chordsGuesserStore = this.store.select(selectChordGuesserState);
+  public chords: Chord[] = [];
+
   ngOnInit(): void {
+    this.chordGuesserSubscription = this.chordsGuesserStore.subscribe(
+      (state) => {
+        if (state && state.currentChords) this.chords = state.currentChords;
+      }
+    );
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.functionSelectedStoreSubscription =
@@ -28,6 +39,7 @@ export class FunctionSelectorComponent {
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
+    this.chordGuesserSubscription.unsubscribe();
     this.functionSelectedStoreSubscription.unsubscribe();
   }
 
