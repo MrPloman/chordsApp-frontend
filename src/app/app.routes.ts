bbb00d89b908f5318791a958373fe26a8c6d070f
MrPloman <polplanaayet@gmail.com>
2026-01-03 09:48:58 +0100

    Routes already defined and handbook displaying modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,25 @@ export const routes: Routes = [
         (c) => c.ChordsGuesserComponent
       ),
   },
+  {
+    path: 'progression',
+    loadComponent: () =>
+      import(
+        './components/chords-progression/chords-progression.component'
+      ).then((c) => c.ChordsProgressionComponent),
+  },
+  {
+    path: 'options',
+    loadComponent: () =>
+      import('./components/chords-options/chords-options.component').then(
+        (c) => c.ChordsOptionsComponent
+      ),
+  },
+  {
+    path: 'handbook',
+    loadComponent: () =>
+      import('./components/chords-handbook/chords-handbook.component').then(
+        (c) => c.ChordsHandbookComponent
+      ),
+  },
 ];
