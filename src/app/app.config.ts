96ee70ab0db4801e58e23f531507fc1d9189ea1f
MrPloman <polplanaayet@gmail.com>
2026-01-15 16:00:59 -0600

    interceptor implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { AIHttpInterceptor } from '@app/interceptors/AiHttpInterceptor.interceptor';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { TranslateModule } from '@ngx-translate/core';
+import { AIHttpInterceptor } from 'interceptors/AiHttpInterceptor.interceptor';
 import { routes } from './app.routes';
 import { chordsReducer } from './store/reducers/chords.reducer';
 import { languageReducer } from './store/reducers/language.reducer';
@@ -14,8 +13,6 @@ import { loadingReducer } from './store/reducers/loading.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(TranslateModule.forRoot()),
-
     provideHttpClient(withInterceptors([AIHttpInterceptor])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
