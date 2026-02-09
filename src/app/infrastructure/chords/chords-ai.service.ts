c7dbfb87a716eaf3992d87d7757eeb009ab0e183
MrPloman <polplanaayet@gmail.com>
2026-02-09 10:20:29 -0600

    changed endpoint name

--- src/app/infrastructure/chords/chords-ai.service.ts
+++ src/app/infrastructure/chords/chords-ai.service.ts
@@ -8,14 +8,14 @@ import { firstValueFrom } from 'rxjs';
 export class AIService implements ChordsAiPort {
   constructor(private http: HttpClient) {}
   public async guessChords(chords: Chord[], language: 'es' | 'en') {
-    const response = await this.http.post<AiResponse>(`${environment.API}/guesser`, {
+    const response = await this.http.post<AiResponse>(`${environment.API_GPT}/guesser`, {
       chords: chords,
       language: language,
     });
     return firstValueFrom(response);
   }
   public async getProgression(chords: Chord[], prompt: string, language: 'es' | 'en') {
-    const response = await this.http.post<AiResponse>(`${environment.API}/progression`, {
+    const response = await this.http.post<AiResponse>(`${environment.API_GPT}/progression`, {
       prompt,
       chords,
       language,
@@ -24,11 +24,11 @@ export class AIService implements ChordsAiPort {
   }
 
   public async getAlternativeChords(chord: Chord) {
-    const response = await this.http.post<AiResponse>(`${environment.API}/options`, { chord });
+    const response = await this.http.post<AiResponse>(`${environment.API_GPT}/options`, { chord });
     return firstValueFrom(response);
   }
   public async getHandbookChords(chordName: string) {
-    const response = await this.http.post<AiResponse>(`${environment.API}/forms`, { chordName });
+    const response = await this.http.post<AiResponse>(`${environment.API_GPT}/forms`, { chordName });
     return firstValueFrom(response);
   }
 }
